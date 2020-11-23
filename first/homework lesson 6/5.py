c86746edd462227ed70f82c7dde402eb767a3006
Ilya <viral2705@gmail.com>
2020-11-23 20:45:55 +0300

    homework 6 add

---
+++ first/homework lesson 6/5.py
@@ -0,0 +1,34 @@
+"""
+Реализовать класс Stationery (канцелярская принадлежность). Определить в нем атрибут title (название) и метод draw (отрисовка).
+Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер).
+В каждом из классов реализовать переопределение метода draw. Для каждого из классов методы должен выводить уникальное сообщение.
+Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
+"""
+
+class Stationery:
+    def __init__(self, title='Канцелярская принадлежость'):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+class Pen(Stationery):
+    def draw(self):
+        print('Рисуем ручкой')
+
+class Pencil(Stationery):
+    def draw(self):
+        print('Рисуем карандашом')
+
+class Handle(Stationery):
+    def draw(self):
+        print('Рисуем маркером')
+
+Stationery = Stationery()
+Stationery.draw()
+Pen = Pen()
+Pen.draw()
+Pencil = Pencil()
+Pencil.draw()
+Handle = Handle()
+Handle.draw()
